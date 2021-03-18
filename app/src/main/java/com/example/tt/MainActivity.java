cf6624a6a8bf8f7aec319cd11f0a4cf0a25752f5
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-03-18 22:35:21 +0530

    Removed scrollview and grouped similar input fields

--- app/src/main/java/com/example/tt/MainActivity.java
+++ app/src/main/java/com/example/tt/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tt;
 
+import android.app.ActionBar;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.ProgressDialog;
@@ -10,6 +11,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -23,15 +25,16 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
-    public static final String Student_name="studentname";
-
+    public static final String Student_name = "studentname";
 
     private EditText email;
     private EditText password;
     private Button btnSubmit;
     private Button btnsignup;
+    private Button next_button;
+    private Button back_button;
     private FirebaseAuth mAuth;
     private Button btncontact;
     private ProgressDialog progressDialog;
@@ -39,15 +42,16 @@ public class MainActivity extends AppCompatActivity  {
     private EditText mobno;
     private EditText roll;
 
-   DatabaseReference databasestudents;
+    private LinearLayout page1;
+    private LinearLayout page2;
 
-
+    DatabaseReference databasestudents;
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        check c=new check(getApplicationContext());
+        check c = new check(getApplicationContext());
         c.firsttime();
     }
 
@@ -55,23 +59,35 @@ public class MainActivity extends AppCompatActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        progressDialog = new ProgressDialog(MainActivity.this);
-        email = (EditText) findViewById(R.id.email);
-        name = (EditText) findViewById(R.id.name);
-        password = (EditText) findViewById(R.id.password);
-        btnSubmit = (Button) findViewById(R.id.login);
 
-        mobno = (EditText) findViewById(R.id.no);
-        roll= (EditText)findViewById(R.id.regd1);
-        btnsignup = (Button) findViewById(R.id.signup);
+        getSupportActionBar().hide();
+
+        progressDialog = new ProgressDialog(MainActivity.this);
+        email = findViewById(R.id.email);
+        name = findViewById(R.id.name);
+        password = findViewById(R.id.password);
+        btnSubmit = findViewById(R.id.login);
+
+        mobno = findViewById(R.id.no);
+        roll = findViewById(R.id.regd1);
+        btnsignup = findViewById(R.id.signup);
+
+        next_button = findViewById(R.id.next);
+        back_button = findViewById(R.id.back);
+
+        page1 = findViewById(R.id.page1);
+        page2 = findViewById(R.id.page2);
+
         databasestudents = FirebaseDatabase.getInstance().getReference("students");
         mAuth = FirebaseAuth.getInstance();
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-            NotificationChannel channel=
-                    new NotificationChannel("MyNotifications","MyNotifications", NotificationManager.IMPORTANCE_DEFAULT);
-            NotificationManager manager=getSystemService(NotificationManager.class);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel =
+                    new NotificationChannel("MyNotifications", "MyNotifications", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
         }
+
         FirebaseMessaging.getInstance().subscribeToTopic("general")
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
@@ -81,17 +97,31 @@ public class MainActivity extends AppCompatActivity  {
                             msg = "Failed";
                         }
                         // Log.d(TAG, msg);
-                       // Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                        // Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
                     }
                 });
 
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                page1.setVisibility(View.VISIBLE);
+                page2.setVisibility(View.GONE);
+                btnsignup.setEnabled(false);
+            }
+        });
 
-
+        next_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                page1.setVisibility(View.GONE);
+                page2.setVisibility(View.VISIBLE);
+                btnsignup.setEnabled(true);
+            }
+        });
 
         btnsignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
 
                 String e = email.getText().toString();
                 String p = password.getText().toString();
@@ -110,10 +140,10 @@ public class MainActivity extends AppCompatActivity  {
                             String N = name.getText().toString();
                             String E = email.getText().toString();
                             String M = mobno.getText().toString();
-                            String R=roll.getText().toString();
+                            String R = roll.getText().toString();
                             String id = databasestudents.push().getKey();
 
-                            studentdatabase studentdatabase = new studentdatabase(id, N,R, E, M);
+                            studentdatabase studentdatabase = new studentdatabase(id, N, R, E, M);
 
                             databasestudents.child(id).setValue(studentdatabase);
 
@@ -154,7 +184,7 @@ public class MainActivity extends AppCompatActivity  {
                             Toast.makeText(MainActivity.this, "Invalid email or password/Register first", Toast.LENGTH_SHORT).show();
                         } else {
 
-                            check c=new check(getApplicationContext());
+                            check c = new check(getApplicationContext());
                             c.secondtime();
                             String N = name.getText().toString();
                             Toast.makeText(MainActivity.this, "logged in", Toast.LENGTH_SHORT).show();
@@ -186,24 +216,21 @@ public class MainActivity extends AppCompatActivity  {
 
 
     }
-    int backButtonCount=0;
+
+    int backButtonCount = 0;
+
     @Override
-    public void onBackPressed()
-    {
-        if(backButtonCount >= 1)
-        {
+    public void onBackPressed() {
+        if (backButtonCount >= 1) {
             Intent intent = new Intent(Intent.ACTION_MAIN);
             intent.addCategory(Intent.CATEGORY_HOME);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
-        }
-        else
-        {
+        } else {
             Toast.makeText(this, "Press the back button once again to close the application.", Toast.LENGTH_SHORT).show();
             backButtonCount++;
         }
     }
-
 
 
 }
