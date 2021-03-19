35f18878157534617a756f7a5203a92ae61d3ca8
Aditya Bhardwaj <63164037+aditya-190@users.noreply.github.com>
2021-03-19 16:27:29 +0530

    Resolved Issue #47
    On Hitting Sign In or Sign Up Button Without entering
    any details make the app to crash.
    
    I have done changes in MainActivity.java in line No. 103 and 153 by wrapping it into the condition so that when the app makes call to firebase authentication it always have email and password and if not pop up toast to ask for the same.

--- app/src/main/java/com/example/tt/MainActivity.java
+++ app/src/main/java/com/example/tt/MainActivity.java
@@ -23,9 +23,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessaging;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
-    public static final String Student_name="studentname";
+    public static final String Student_name = "studentname";
 
 
     private EditText email;
@@ -39,15 +39,14 @@ public class MainActivity extends AppCompatActivity  {
     private EditText mobno;
     private EditText roll;
 
-   DatabaseReference databasestudents;
-
+    DatabaseReference databasestudents;
 
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        check c=new check(getApplicationContext());
+        check c = new check(getApplicationContext());
         c.firsttime();
     }
 
@@ -62,14 +61,14 @@ public class MainActivity extends AppCompatActivity  {
         btnSubmit = (Button) findViewById(R.id.login);
 
         mobno = (EditText) findViewById(R.id.no);
-        roll= (EditText)findViewById(R.id.regd1);
+        roll = (EditText) findViewById(R.id.regd1);
         btnsignup = (Button) findViewById(R.id.signup);
         databasestudents = FirebaseDatabase.getInstance().getReference("students");
         mAuth = FirebaseAuth.getInstance();
-        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-            NotificationChannel channel=
-                    new NotificationChannel("MyNotifications","MyNotifications", NotificationManager.IMPORTANCE_DEFAULT);
-            NotificationManager manager=getSystemService(NotificationManager.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel =
+                    new NotificationChannel("MyNotifications", "MyNotifications", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel);
         }
         FirebaseMessaging.getInstance().subscribeToTopic("general")
@@ -81,11 +80,9 @@ public class MainActivity extends AppCompatActivity  {
                             msg = "Failed";
                         }
                         // Log.d(TAG, msg);
-                       // Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                        // Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
                     }
                 });
-
-
 
 
         btnsignup.setOnClickListener(new View.OnClickListener() {
@@ -96,37 +93,46 @@ public class MainActivity extends AppCompatActivity  {
                 String e = email.getText().toString();
                 String p = password.getText().toString();
 
-                progressDialog.setMessage("Registering user");
-                progressDialog.show();
+                if (TextUtils.isEmpty(e)) {
+                    Toast.makeText(MainActivity.this, "please enter email", Toast.LENGTH_SHORT).show();
+                }
+                if (TextUtils.isEmpty(p)) {
+                    Toast.makeText(MainActivity.this, "please enter password", Toast.LENGTH_SHORT).show();
+                }
 
-                mAuth.createUserWithEmailAndPassword(e, p).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (!task.isSuccessful()) {
-                            String mssg = task.getException().getMessage();
-                            Toast.makeText(MainActivity.this, mssg, Toast.LENGTH_SHORT).show();
-                            progressDialog.dismiss();
-                        } else {
-                            String N = name.getText().toString();
-                            String E = email.getText().toString();
-                            String M = mobno.getText().toString();
-                            String R=roll.getText().toString();
-                            String id = databasestudents.push().getKey();
+                if (!TextUtils.isEmpty(e) && !TextUtils.isEmpty(p)) {
+                    progressDialog.show();
+                    progressDialog.setMessage("Registering user");
 
-                            studentdatabase studentdatabase = new studentdatabase(id, N,R, E, M);
+                    mAuth.createUserWithEmailAndPassword(e, p).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                String mssg = task.getException().getMessage();
+                                Toast.makeText(MainActivity.this, mssg, Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                            } else {
+                                String N = name.getText().toString();
+                                String E = email.getText().toString();
+                                String M = mobno.getText().toString();
+                                String R = roll.getText().toString();
+                                String id = databasestudents.push().getKey();
 
-                            databasestudents.child(id).setValue(studentdatabase);
+                                studentdatabase studentdatabase = new studentdatabase(id, N, R, E, M);
+
+                                databasestudents.child(id).setValue(studentdatabase);
 
 
-                            progressDialog.setMessage("Registering user");
-                            Toast.makeText(MainActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
-                            progressDialog.dismiss();
+                                progressDialog.setMessage("Registering user");
+                                Toast.makeText(MainActivity.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
 
+
+                            }
 
                         }
-
-                    }
-                });
+                    });
+                }
             }
         });
 
@@ -144,29 +150,32 @@ public class MainActivity extends AppCompatActivity  {
                 if (TextUtils.isEmpty(p)) {
                     Toast.makeText(MainActivity.this, "please enter password", Toast.LENGTH_SHORT).show();
                 }
-                progressDialog.show();
 
-                mAuth.signInWithEmailAndPassword(e, p).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        progressDialog.dismiss();
-                        if (!task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "Invalid email or password/Register first", Toast.LENGTH_SHORT).show();
-                        } else {
+                if (!TextUtils.isEmpty(e) && !TextUtils.isEmpty(p)) {
+                    progressDialog.show();
 
-                            check c=new check(getApplicationContext());
-                            c.secondtime();
-                            String N = name.getText().toString();
-                            Toast.makeText(MainActivity.this, "logged in", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(getApplicationContext(), PROFILE.class);
+                    mAuth.signInWithEmailAndPassword(e, p).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "Invalid email or password/Register first", Toast.LENGTH_SHORT).show();
+                            } else {
 
-                            intent.putExtra(Student_name, N);
-                            startActivity(intent);
+                                check c = new check(getApplicationContext());
+                                c.secondtime();
+                                String N = name.getText().toString();
+                                Toast.makeText(MainActivity.this, "logged in", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(), PROFILE.class);
+
+                                intent.putExtra(Student_name, N);
+                                startActivity(intent);
 
 
+                            }
+                            progressDialog.dismiss();
                         }
-                    }
-                });
+                    });
+                }
             }
         });
 
@@ -186,24 +195,21 @@ public class MainActivity extends AppCompatActivity  {
 
 
     }
-    int backButtonCount=0;
+
+    int backButtonCount = 0;
+
     @Override
-    public void onBackPressed()
-    {
-        if(backButtonCount >= 1)
-        {
+    public void onBackPressed() {
+        if (backButtonCount >= 1) {
             Intent intent = new Intent(Intent.ACTION_MAIN);
             intent.addCategory(Intent.CATEGORY_HOME);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(intent);
-        }
-        else
-        {
+        } else {
             Toast.makeText(this, "Press the back button once again to close the application.", Toast.LENGTH_SHORT).show();
             backButtonCount++;
         }
     }
-
 
 
 }
