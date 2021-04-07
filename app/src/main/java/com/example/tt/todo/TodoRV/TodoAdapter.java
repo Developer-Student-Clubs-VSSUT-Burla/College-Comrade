0dc29bac4cd09e61c7353c421ba5987a7b49c393
Sitam Sardar <76570320+sitamadex11@users.noreply.github.com>
2021-04-07 08:44:17 +0530

    modified 7 april

--- app/src/main/java/com/example/tt/todo/TodoRV/TodoAdapter.java
+++ app/src/main/java/com/example/tt/todo/TodoRV/TodoAdapter.java
@@ -4,11 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-<<<<<<< HEAD
-=======
 import android.widget.CompoundButton;
 import android.widget.RadioGroup;
->>>>>>> upstream/master
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -16,11 +13,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tt.R;
 import com.example.tt.todo.RoomDb.Todo;
-<<<<<<< HEAD
-=======
 import com.example.tt.todo.RoomDb.TodoDatabase;
 import com.google.android.material.checkbox.MaterialCheckBox;
->>>>>>> upstream/master
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -33,22 +27,16 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder
     ArrayList<Todo> allNotes = new ArrayList<>();
     Context context;
 
-<<<<<<< HEAD
-=======
 
->>>>>>> upstream/master
     public TodoAdapter(Context context) {
         this.context = context;
     }
 
-<<<<<<< HEAD
-=======
     @Override
     public long getItemId(int position) {
         return allNotes.get(position).getId();
     }
 
->>>>>>> upstream/master
     @NonNull
     @Override
     public TodoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -65,16 +53,10 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder
         holder.tvDescription.setText(currentTodo.getDescription());
         updateTime(holder, currentTodo);
         updateDate(holder, currentTodo);
-<<<<<<< HEAD
-       int[] colors= holder.itemView.getResources().getIntArray(R.array.random_color);
-       int randomColor = colors[new Random().nextInt(colors.length)];
-       holder.viewColorTag.setBackgroundColor(randomColor);
-=======
         int[] colors= holder.itemView.getResources().getIntArray(R.array.random_color);
         int randomColor = colors[new Random().nextInt(colors.length)];
         holder.viewColorTag.setBackgroundColor(randomColor);
 
->>>>>>> upstream/master
 
     }
 
@@ -123,7 +105,3 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.TodoViewHolder
         }
     }
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> upstream/master
