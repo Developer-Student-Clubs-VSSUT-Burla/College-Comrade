0cec127c2d37de1cb1ea412125d9625b616da910
Sitam Sardar <76570320+sitamadex11@users.noreply.github.com>
2021-05-03 13:21:35 +0530

    serach functionality resolved

--- app/src/main/java/com/example/tt/PdfReaderActivity.java
+++ app/src/main/java/com/example/tt/PdfReaderActivity.java
@@ -1,95 +1,105 @@
 package com.example.tt;
 
-import android.content.pm.PackageManager;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.os.Environment;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.MotionEvent;
+
+import android.widget.Toast;
 
 import com.example.tt.adapter.PdfListAdapter;
 import com.skydoves.androidveil.VeilRecyclerFrameView;
 
 import java.io.File;
 import java.util.ArrayList;
-
-import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+import java.util.concurrent.Executor;
 
 public class PdfReaderActivity extends AppCompatActivity {
-    private static final int PERMISSION_REQUEST_CODE = 200;
-    //    private static ArrayList<PDFInfo> pdfInfoList=new ArrayList<>();
-    public static ArrayList<File> pdfFileList = new ArrayList<>();
+    private static final int PERMISSION_REQUEST_CODE=200;
+//    private static ArrayList<PDFInfo> pdfInfoList=new ArrayList<>();
+    public static ArrayList<File> pdfFileList=new ArrayList<>();
     VeilRecyclerFrameView rvPdfList;
     SearchView itemSearch;
-    PdfListAdapter adapter;
-
+    PdfListAdapter adapter= new PdfListAdapter(PdfReaderActivity.this);;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf_reader);
-        itemSearch = findViewById(R.id.itemSearch);
-        rvPdfList = findViewById(R.id.rvPdfList);
+        itemSearch=findViewById(R.id.itemSearch);
+        rvPdfList=findViewById(R.id.rvPdfList);
         rvPdfList.setLayoutManager(new LinearLayoutManager(PdfReaderActivity.this));
         rvPdfList.addVeiledItems(15);
-        if (!checkPermission()) {
+        if(!checkPermission()){
             requestPermission();
-        } else {
+        }else {
             File dir = new File(Environment.getExternalStorageDirectory().toString());
             System.out.println(dir);
             AsyncTaskRunner runner = new AsyncTaskRunner();
             runner.execute(dir);
-            adapter = new PdfListAdapter(PdfReaderActivity.this, pdfFileList);
             rvPdfList.setAdapter(adapter);
             System.out.println(pdfFileList.size());
-            //adapter.updateList(pdfFileList);
         }
-        itemSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                //todo
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                if (!newText.isEmpty()) {
-                    adapter.getFilter().filter(newText);
+            itemSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    //todo
+                    return true;
                 }
-                return false;
-            }
-        });
 
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                 //   Toast.makeText(PdfReaderActivity.this, newText, Toast.LENGTH_SHORT).show();
+                    if (!newText.isEmpty()) {
+                        adapter.getFilter().filter(newText);
+                    }
+                    return false;
+                }
+            });
     }
 
     private class AsyncTaskRunner extends AsyncTask<File, String, ArrayList<File>> {
         @Override
         protected ArrayList<File> doInBackground(File... params) {
-            return getfile(params[0]);
+            pdfFileList.clear();
+            pdfFileList.addAll(getfile(params[0]));
+            return pdfFileList;
         }
 
 
         @Override
         protected void onPostExecute(ArrayList<File> files) {
             super.onPostExecute(files);
+            adapter.updateList(files);
             rvPdfList.unVeil();
         }
 
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
-            rvPdfList.veil();
+           rvPdfList.veil();
+        }
+
+
+        @Override
+        protected void onProgressUpdate(String... text) {
+
+
         }
     }
-
     private ArrayList<File> getfile(File dir) {
-        File listFile[] = dir.listFiles();
+        File listFile[]=dir.listFiles();
         File f = new File(String.valueOf(dir));
         System.out.println(f.isDirectory());
         if (listFile != null && listFile.length > 0) {
@@ -100,7 +110,8 @@ public class PdfReaderActivity extends AppCompatActivity {
                     getfile(listFile[i]);
 
                 } else {
-                    if (listFile[i].getName().endsWith(".pdf")) {
+                    if (listFile[i].getName().endsWith(".pdf"))
+                    {
                         pdfFileList.add(listFile[i]);
                     }
                 }
@@ -111,24 +122,23 @@ public class PdfReaderActivity extends AppCompatActivity {
 
     private void requestPermission() {
         ActivityCompat.requestPermissions(this,
-                new String[]{READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE},
+                new String[]{READ_EXTERNAL_STORAGE,WRITE_EXTERNAL_STORAGE},
                 PERMISSION_REQUEST_CODE);
     }
 
     private boolean checkPermission() {
-        int writePermission = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
-        int readPermission = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
-        return writePermission == PackageManager.PERMISSION_GRANTED && readPermission == PackageManager.PERMISSION_GRANTED;
+        int writePermission= ContextCompat.checkSelfPermission(getApplicationContext(),WRITE_EXTERNAL_STORAGE);
+        int readPermission= ContextCompat.checkSelfPermission(getApplicationContext(),READ_EXTERNAL_STORAGE);
+        return writePermission== PackageManager.PERMISSION_GRANTED && readPermission==PackageManager.PERMISSION_GRANTED;
     }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                Toast.makeText(this.getApplicationContext(), "Permission granted.", Toast.LENGTH_SHORT).show();
+        if(requestCode==PERMISSION_REQUEST_CODE){
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED)
+                Toast.makeText(this.getApplicationContext(),"Permission granted.",Toast.LENGTH_SHORT).show();
             else
-                Toast.makeText(this.getApplicationContext(), "Permission denied.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this.getApplicationContext(),"Permission denied.",Toast.LENGTH_SHORT).show();
         }
     }
 }
--- app/src/main/java/com/example/tt/adapter/PdfListAdapter.java
+++ app/src/main/java/com/example/tt/adapter/PdfListAdapter.java
@@ -2,6 +2,7 @@ package com.example.tt.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,19 +16,21 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.tt.PdfViewerActivity;
 import com.example.tt.R;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PdfListAdapter extends RecyclerView.Adapter<PdfListAdapter.PdfListViewHolder> implements Filterable {
     Context context;
-    ArrayList<File> list;
+    public ArrayList<File> list=new ArrayList<>();
     ArrayList<File> allList;
-    public PdfListAdapter(Context context,ArrayList<File>list){
-        this.list=list;
+    public PdfListAdapter(Context context){
         this.context=context;
-        allList=new ArrayList<File>(list);
+       // allList=new ArrayList<File>(list);
     }
+    @NotNull
     @Override
     public PdfListViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
         View view= LayoutInflater.from(context).inflate(R.layout.pdf_list_item,parent,false);
@@ -37,7 +40,6 @@ public class PdfListAdapter extends RecyclerView.Adapter<PdfListAdapter.PdfListV
     @Override
     public void onBindViewHolder( PdfListViewHolder holder, int position) {
         holder.pdfName.setText(list.get(position).getName());
-        System.out.println(list.get(position).getTotalSpace());
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,20 +56,23 @@ public class PdfListAdapter extends RecyclerView.Adapter<PdfListAdapter.PdfListV
         return list.size();
     }
 
-    void updateList(ArrayList newlist){
+   public void updateList(ArrayList newlist){
         list.clear();
         list.addAll(newlist);
+        allList=new ArrayList<>(list);
         notifyDataSetChanged();
     }
 
     @Override
     public Filter getFilter() {
+        Log.d("list size", String.valueOf(allList.size()));
         return exampleFilter;
     }
     private final Filter exampleFilter=new Filter() {
         @Override
         protected FilterResults performFiltering(CharSequence constraint) {
             ArrayList<File> sample = new ArrayList<>();
+
             if (constraint == null || constraint.length() == 0) {
                 sample.addAll(allList);
             } else {
@@ -87,7 +92,7 @@ public class PdfListAdapter extends RecyclerView.Adapter<PdfListAdapter.PdfListV
         @Override
         protected void publishResults(CharSequence constraint, FilterResults results) {
             list.clear();
-            list.addAll((List)results.values);
+            list.addAll((ArrayList<File>)results.values);
             notifyDataSetChanged();
         }
     };
